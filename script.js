8a56f30c4a98bc0e995775e24ee14f572a02f1cf
Gasper Katrasnik <gkatrasnik@gmail.com>
2020-11-24 12:45:24 +0100

    changed functions a bit, woring delete book button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let myLibrary = [];
 
+
 //constructor
 function book(title, author, pages, read) {
     this.title = title;
@@ -12,69 +13,83 @@ function book(title, author, pages, read) {
     }
   }
 
-function addBookToLibrary(title, author, pages, read) {
-  let newBook = new book(title, author, pages, read);
-  myLibrary.unshift(newBook);
-  displayLibrary(myLibrary);
-}
-
-function displayLibrary(myLibrary) {
-  const container = document.getElementById("container");
-  let bookCard = document.createElement("div"); 
-  
-    let title = myLibrary[0].title;
-    let author = myLibrary[0].author;
-    let pages = myLibrary[0].pages;
-    let read = myLibrary[0].read;
-      
-    bookCard.id = myLibrary[0].title;
-    bookCard.classList.add("lib-element");
-
-    let textTitle = document.createElement("p"); 
-    textTitle.textContent= title; 
-
-    let textAuthor = document.createElement("p");
-    textAuthor.textContent= author; 
-
-    let textPages = document.createElement("p");
-    textPages.textContent = ("Pages: " + pages);  
-
-    let label = document.createElement("p");
-    label.textContent = "Read:"; 
-
-    readCheck = document.createElement("INPUT");
-    readCheck.setAttribute("id", "read");
-    readCheck.setAttribute("type", "checkbox");
-    readCheck.checked = read; 
-
-    let removeButton = document.createElement("button");
-    removeButton.setAttribute("class", "removeBook");
-    removeButton.textContent = "Remove"
-
-    
-    bookCard.appendChild(textTitle);
-    bookCard.appendChild(textAuthor);
-    bookCard.appendChild(textPages);
-    bookCard.appendChild(label);
-    bookCard.appendChild(readCheck);
-    bookCard.appendChild(removeButton);  
-  
-    container.appendChild(bookCard);
-}
 
 
 
-function createNewBook() {
+
+function addBookToLibrary() {
+
   const bookTitle = document.getElementById("book-title").value;
   const bookAuthor = document.getElementById("book-author").value;
   const bookPages = document.getElementById("book-pages").value;
   const bookRead = document.getElementById("book-read").checked;
-    
-  addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead);  
-  
+
+  let newBook = new book(bookTitle, bookAuthor, bookPages, bookRead);
+  myLibrary.unshift(newBook);
+  render();
   popupShow();
+  console.log("clicked button")
 }
 
+function render() {
+
+  const container = document.getElementById("container");
+  container.innerHTML =""; //pobriše vse 
+
+  for (let i in myLibrary) { //za vsako knjigo v arrayu nardi nov card
+    createBook(myLibrary[i]);
+  }  
+}
+
+function createBook(item) {
+  let bookCard = document.createElement("div");  
+  bookCard.classList.add("lib-element");
+
+  let textTitle = document.createElement("p"); 
+  textTitle.textContent= item.title; 
+
+  let textAuthor = document.createElement("p");
+  textAuthor.textContent= item.author; 
+
+  let textPages = document.createElement("p");
+  textPages.textContent = ("Pages: " + item.pages);  
+
+  let label = document.createElement("p");
+  label.textContent = "Read:"; 
+
+  readCheck = document.createElement("INPUT");
+  readCheck.setAttribute("id", "read");
+  readCheck.setAttribute("type", "checkbox");
+  readCheck.checked = item.read; 
+
+  let removeButton = document.createElement("button");
+  removeButton.setAttribute("class", "removeBook");
+  removeButton.textContent = "Remove";
+
+  removeButton.addEventListener("click", () => {
+    myLibrary.splice(myLibrary.indexOf(item), 1);
+    render();
+  })
+
+  
+  bookCard.appendChild(textTitle);
+  bookCard.appendChild(textAuthor);
+  bookCard.appendChild(textPages);
+  bookCard.appendChild(label);
+  bookCard.appendChild(readCheck);
+  bookCard.appendChild(removeButton);  
+  container.appendChild(bookCard);
+  
+}
+
+function removeCard() {
+
+  
+  //let cards = document.querySelectorAll(".lib-element");  
+  //cardId = cards.dataset.index;
+  console.log(card);
+
+}
 
 function popupShow() {
     let element = document.getElementById("popupDiv")
@@ -94,4 +109,4 @@ function popupClear() {
 
 const addButton = document.querySelector("#add");
 
-addButton.addEventListener("click", createNewBook);
+addButton.addEventListener("click", addBookToLibrary);
